36613acf6db248291f2c89997433e98eae9d9d66
siwoo1313 <siwoo2762@gmail.com>
2025-04-02 09:06:21 +0900

    0331

--- Lec04_Array/Lec04_Array/소스.cpp
+++ Lec04_Array/Lec04_Array/소스.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX_SIZE 10
 
 void sub(int x, int arr[]) {
---
+++ Lec04_Array/Lec04_Class/main.cpp
@@ -0,0 +1,1 @@
+#include "Complex.h"#include "Car.h"int main(){	Complex a, b, c;	a.read("A= ");	a.print("A = ");	Car myCar;	Car momsCar(10, "K5", 2);	myCar.whereAmI();	momsCar.whereAmI();	myCar.changeGear(3);	myCar.display();}
