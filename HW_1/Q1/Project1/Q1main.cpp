1bcfc25db148914b49feebaffe5666db6d7cc021
siwoo1313 <siwoo2762@gmail.com>
2025-04-15 19:32:37 +0900

    new start-5

--- HW_1/Q1/Project1/Q1main.cpp
+++ HW_1/Q1/Project1/Q1main.cpp
@@ -1,9 +1,20 @@
+#include <iostream>  
+using namespace std; 
+
+int findMax(int arr[], int size) { //오류때문에 헤더 파일에 작성하지 않고 소스 파일에 작성하였음
+    if (size <= 0) return -1;
+    int maxVal = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+    return maxVal;
+}
+
 int main() {
-    int numbers[] = { -4, 7, 0, 30, 1 };
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int arr[] = { -4, 7, 0, 30, 1 };
+    int maxValue = findMax(arr, 5);
 
-    int maxNumber = findMax(numbers, size);
-    cout << "최댓값: " << maxNumber << endl;
-
+    cout << "최댓값: " << maxValue << endl;
     return 0;
 }
